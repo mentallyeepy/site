d82c1ac1c38da0d037a9dbd7f7095792ae7badfe
mentallyeepy <ethan.angelo.sanchez11@gmail.com>
2026-01-27 16:57:44 -0700

    Fix filename case in window.open for directory

--- Content/dir.js
+++ Content/dir.js
@@ -4,7 +4,7 @@ const btn = document.getElementById('directoryButton');
 btn.addEventListener('click', () => {
     // Opens the directory in a small independent window
     window.open(
-        'directory.html', 
+        'Directory.html', 
         'SiteDirectory', 
         'width=350,height=450,resizable=yes'
     );
