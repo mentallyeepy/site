40272845b7ad13ed02ab0798aab3ff3e5692823e
mentallyeepy <ethan.angelo.sanchez11@gmail.com>
2026-01-27 16:43:36 -0700

    Add function to open directory in a new window

---
+++ Content/dir.js
@@ -0,0 +1,8 @@
+function openDirectory() {
+  // Opening with specific width/height forces it into a separate window
+  window.open(
+    'directory.html', 
+    'SiteDirectory', 
+    'width=400,height=500,resizable=yes,scrollbars=yes'
+  );
+}
